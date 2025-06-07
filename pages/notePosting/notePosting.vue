b5b222c70645c2f68eec9cd00780ec171ea65a30
applekkkk <pilot12334@proton.me>
2025-06-07 22:17:27 +0800

    加入文心一言

--- pages/notePosting/notePosting.vue
+++ pages/notePosting/notePosting.vue
@@ -18,6 +18,7 @@
 			<textarea class="textarea-content" v-model="content" placeholder="添加正文..." />
 
 			<!-- 发布按钮 -->
+			<button class="submit-btn" @click="AIhelp">AI帮写</button>
 			<button class="submit-btn" @click="submit">发布动态</button>
 		</view>
 	</scroll-view>
@@ -25,10 +26,58 @@
 <script setup>
 import { ref } from 'vue'
 import { request } from '../../utils/request'
+const idKey="9p3t5sTAkTzYICb2PFWNiU6B"
+const secretKey="J6wiUerTBqXdsmyI5AsgNFvPdHrqdVJi"
 const images = ref([])
 const title = ref('')
 const content = ref('')
 const id=uni.getStorageSync("id")
+const AIhelp = async () => {
+    if (!title.value) {
+        uni.showToast({ title: '请输入标题', icon: 'none' });
+        return;
+    }
+	try {
+			uni.showLoading({
+				title: 'AI思考中'
+			});
+
+	        const response = await uni.request({
+	            url: "https://qianfan.baidubce.com/v2/chat/completions",
+	            data: {
+	                model: "ernie-4.5-turbo-128k", // 确保模型名称正确
+	                messages: [
+	                    { "role": "user", "content":`我要发布一个帖子，以${title.value}未标题，帮我写出帖子的内容` }
+	                ],
+	                web_search: {
+	                    enable: false,
+	                    enable_citation: false,
+	                    enable_trace: false
+	                },
+	                plugin_options: {}
+	            },
+	            method: 'POST',
+	            header: {
+	                'Content-Type': 'application/json',
+	                'appid': '', // 请填写你的 appid
+	                'Authorization': 'Bearer bce-v3/ALTAK-TrhN5ossPB0S4VUGQ6kbP/3d10eeee9fe462b611dbe840e7d5c89edca84304' // 请填写你的 Bearer token
+	            }
+	        });
+	
+	 
+	        // 处理响应
+	        if (response.statusCode === 200) {
+	            console.log('Success:', response.data.choices[0].message.content);
+				uni.hideLoading()
+				content.value=response.data.choices[0].message.content
+	        } else {
+	            console.error('Error:', response.data);
+	        }
+	    } catch (error) {
+	        console.error('Request Error:', error);
+	    }
+};
+ 
 function chooseImage() {
 	uni.chooseImage({
 		count: 9,
